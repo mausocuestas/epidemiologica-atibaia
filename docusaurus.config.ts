5be2fe447c181a624e25497585750f4d380d7329
mausocuestas <alcatoobr@yahoo.com.br>
2023-11-07 17:25:51 -0300

    alta de recem nascido

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DVE-Atibaia',
+  tagline: 'Mosquitos are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -13,11 +13,6 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -25,16 +20,18 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'pt-br',
+    locales: ['pt-br'],
   },
-
+// para navbar: 1) presets para definir o doc padrão 2) plugin-content-docs 3) navbar
   presets: [
     [
       'classic',
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          routeBasePath: 'docs',
+          path: 'docs',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -54,30 +51,70 @@ const config: Config = {
     ],
   ],
 
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'forms',
+        path: 'forms',
+        routeBasePath: 'forms',
+        sidebarPath: require.resolve('./sidebars.js'),
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'orienta',
+        path: 'orienta',
+        routeBasePath: 'orienta',
+        sidebarPath: require.resolve('./sidebars.js'),
+      },
+    ],
+    
+  ],
+
   themeConfig: {
+    announcementBar: {
+      id: 'support_us',
+      content:
+        'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+      backgroundColor: '#fafbfc',
+      textColor: '#091E42',
+      isCloseable: false,
+    },
+
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Home',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Logo da Epidemiológica Atibaia',
         src: 'img/logo.svg',
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          to: '/forms/intro',    // ./docs/Intro.md
+          label: 'Formulários',
           position: 'left',
-          label: 'Tutorial',
+          activeBaseRegex: `/forms/`,
+        },
+        {
+          to: '/docs/intro',    // ./docs-api/Intro.md
+          label: 'Documentos',
+          position: 'left',
+          activeBaseRegex: `/docs/`,
+        },
+        {
+          to: '/orienta/intro',  // ./docs-system/Intro.md
+          label: 'Orientações',
+          position: 'left',
+          activeBaseRegex: `/orienta/`,
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
+
+
     footer: {
       style: 'dark',
       links: [
@@ -85,7 +122,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Formulários',
               to: '/docs/intro',
             },
           ],
@@ -108,24 +145,25 @@ const config: Config = {
           ],
         },
         {
-          title: 'More',
+          title: 'Mais',
           items: [
             {
               label: 'Blog',
               to: '/blog',
             },
             {
-              label: 'GitHub',
+              label: 'Forms',
               href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Epidemiológica Atibaia Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      defaultLanguage: 'javascript',
     },
   } satisfies Preset.ThemeConfig,
 };
